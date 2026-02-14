955efe9655842a26d6183a88178b8c03a8d88cb1
dfsgfdgdfhhh1-a11y <dfsgfdgdfhhh1@gmail.com>
2026-02-14 16:51:36 +0000

    refactor: remove ui and update prefect

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import logfire
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import HTMLResponse, JSONResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+from fastapi.responses import JSONResponse
 
 from api.routers import chat, document, health, session
 from db.sessions import async_engine
@@ -17,9 +15,6 @@ logfire.instrument_fastapi(app=app)
 logfire.instrument_sqlalchemy(engine=async_engine)
 logfire.instrument_redis()
 logfire.instrument_pydantic_ai()
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
 
 app.add_middleware(
     middleware_class=CORSMiddleware,
@@ -49,15 +44,3 @@ app.include_router(router=health.router)
 app.include_router(router=document.router)
 app.include_router(router=session.router)
 app.include_router(router=chat.router)
-
-
-@app.get(path="/")
-async def main_page(request: Request) -> HTMLResponse:
-    return templates.TemplateResponse(name="index.html", context={"request": request})
-
-
-@app.get(path="/chat/{document_id}")
-async def chat_page(request: Request, document_id: int) -> HTMLResponse:
-    return templates.TemplateResponse(
-        name="chat.html", context={"request": request, "document_id": document_id}
-    )
